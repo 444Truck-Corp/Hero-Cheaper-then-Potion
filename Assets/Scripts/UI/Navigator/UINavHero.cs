016e771c96b5c82aedb923d783c4162a7196cc6f
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-03-30 19:02:37 +0900

    refactor : UINavHero

--- Assets/Scripts/UI/Navigator/UINavHero.cs
+++ Assets/Scripts/UI/Navigator/UINavHero.cs
@@ -1,6 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using UnityEngine;
 
 public class UINavHero : UIBase
 {
+    [Header("list obj")]
+    [SerializeField] private Transform ListParent;
+    [SerializeField] private GameObject ListPrefab;
 
+    private Dictionary<BigInteger, SlotHeroList> heroSlots = new();
+    private Dictionary<BigInteger, HeroData> heroInfos = new();
+
+    private readonly int summonPrice = 100;
+
+    public override void Opened(object[] param)
+    {
+        FetchHeroList();
+    }
+
+    public void OnHeroSlotSelected(BigInteger listIdx)
+    {
+        UIManager.Show<UIPopupHeroDetail>(heroInfos[listIdx]);
+    }
+
+    public void OnSummonHeroClicked()
+    {
+        int gold = SaveManager.Instance.MySaveData.gold;
+
+        if (gold >= summonPrice)
+        {
+            gold -= summonPrice;
+            SaveManager.Instance.SetSaveData(nameof(SaveManager.Instance.MySaveData.gold), gold);
+            SpawnManager.Instance.SpawnNewHeros(++SaveManager.Instance.MySaveData.heroNum);
+            FetchHeroList();
+        }
+        else
+        {
+            //TODO : 경고 Popup.
+        }
+    }
+
+    #region Sub Methods
+    private void FetchHeroList()
+    {
+        Dictionary<BigInteger, HeroData> heros = SaveManager.Instance.MySaveData.ownedHeros;
+        if (!heroInfos.SequenceEqual(heros))
+        {
+            foreach (Transform child in ListParent)
+            {
+                Destroy(child.gameObject); //List 초기화
+            }
+
+            heroInfos = heros;
+
+            foreach (var (key, heroData) in heros)
+            {
+                SlotHeroList newSlot = Instantiate(ListPrefab, ListParent).GetComponent<SlotHeroList>();
+                heroSlots.Add(key, newSlot);
+                newSlot.InitHeroSlot(key, heroData);
+            }
+        }
+    }
+    #endregion
 }
