31cd02b0e0ff52575457af8614de911015e8b364
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-04-13 16:17:17 +0900

    update : slot 호출 방법 변경

--- Assets/Scripts/Data/SaveData.cs
+++ Assets/Scripts/Data/SaveData.cs
@@ -6,20 +6,21 @@ using System.Numerics;
 public class SaveData
 {
     #region common
-    public int gold = 0;
+    public long lastSaveTime = 0; 
     public int day = 100;
     public float time = 0; //0은 0시, 3분은 24시 (저장 단위 : 초)
-    public int[] remainDay; // 훈련 후 남은 누적일. 방 4개가 상이
+    public int gold = 0;
+    public int guildLevel = 1; //길드 레벨
     #endregion
 
     #region hero 
     public int heroNum = 0; //Hero Id 매기는 변수.
-    public Dictionary<BigInteger, HeroData> ownedHeros; //보유 Hero 목록.
+    public Dictionary<BigInteger, HeroData> ownedHeros = new(); //보유 Hero 목록.
     #endregion
 
     #region inventory
-    public Dictionary<int, int> potionCounts;
-    public List<EquipData> ownedEquips;
+    public Dictionary<int, int> potionCounts = new();
+    public List<EquipData> ownedEquips = new();
     #endregion
 
     #region progress
