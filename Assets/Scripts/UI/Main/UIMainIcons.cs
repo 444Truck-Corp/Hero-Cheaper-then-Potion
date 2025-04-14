ba337aefb4df456689b1fbe512d628891537f1ed
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-04-14 21:12:02 +0900

    feat : 메뉴로 돌아갈 때 Modal 띄우기

--- Assets/Scripts/UI/Main/UIMainIcons.cs
+++ Assets/Scripts/UI/Main/UIMainIcons.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -15,6 +16,18 @@ public class UIMainIcons : MonoBehaviour
     }
 
     public void OnHomeClicked()
+    {
+        UIManager.Show<UIOverrideModal>(
+            "메인 메뉴로",
+            "정말 메인 메뉴로 돌아가시겠습니까?",
+            true,
+            true,
+            (Action)BackToMenu
+        );
+
+    }
+
+    private void BackToMenu()
     {
         SaveManager.Instance.SaveSlot(0);
         SceneManager.LoadScene("StartScene");
