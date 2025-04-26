b3c55e28546e193dd927f1869e160fd221014deb
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-04-26 13:35:57 +0900

    update : 앞에 UI가 있을 때 Scene 오브젝트 클릭되지 않게 변경

--- Assets/Scripts/TileMap/MonoButtonHandler.cs
+++ Assets/Scripts/TileMap/MonoButtonHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MonoButtonHandler : MonoBehaviour
 {
@@ -6,11 +7,15 @@ public class MonoButtonHandler : MonoBehaviour
 
     private void Awake()
     {
-        _camera = _camera == null ? Camera.main : _camera;
+        _camera ??= Camera.main;
     }
 
     private void Update()
     {
+        //UI 오브젝트 감지
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+            return;
+
         var worldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
 
         // 현재는 가장 위에 있는 것으로 가져오지만 수정필요
@@ -18,6 +23,7 @@ public class MonoButtonHandler : MonoBehaviour
         if (collider != null && collider.TryGetComponent(out MonoButton button))
         {
             Debug.Log(collider.gameObject.name);
+
             if (Input.GetMouseButtonDown(0))
             {
                 button.OnLeftClick();
