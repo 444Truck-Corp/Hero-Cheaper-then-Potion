559427cab3db07a27fc3b61ea8eb486fd51eb783
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-08-21 15:04:59 +0900

    update : 별출력 함수 수정, hp출력 함수 추가, horizontal 업데이트 함수 추가

--- Assets/Scripts/Util/Utils.cs
+++ Assets/Scripts/Util/Utils.cs
@@ -1,3 +1,8 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 public static class Utils
 {
     public static string ChangeToRomanNumeral(int number)
@@ -9,13 +14,36 @@ public static class Utils
         return thousands[number / 1000] + hundreds[(number % 1000) / 100] + tens[(number % 100) / 10] + ones[number % 10];
     }
 
+    private const char filledStar = '★';
+    private const char blankStar = '☆';
     public static string ChangeToStars(int number)
     {
         string stars = "";
 
-        for (int i = 0; i < number / 2; i++) stars += "★";
-        if (number % 2 == 1) stars += "☆";
+        for (int i = 0; i < number / 2; i++) stars += filledStar;
+        if (number % 2 == 1) stars += blankStar;
 
         return stars;
+    }
+
+    private const char filledHp = '■';
+    private const char blankHp = '□';
+    private const int hpSlot = 10;
+    public static string BuildHpBar(HeroData hero)
+    {
+        if (hero.maxHP <= 0) return new string(blankHp, hpSlot);
+
+        int filled = Mathf.RoundToInt((float)hero.curHP / hero.maxHP * hpSlot);
+        filled = Mathf.Clamp(filled, 0, hpSlot);
+
+        return new string(filledHp, filled) + new string(blankHp, hpSlot - filled);
+    }
+
+    public static void RefreshHorizontalRow(Transform parentHLG, TMP_Text changedText)
+    {
+        changedText.ForceMeshUpdate();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(changedText.rectTransform);
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)parentHLG);
+        Canvas.ForceUpdateCanvases();
     }
 }
