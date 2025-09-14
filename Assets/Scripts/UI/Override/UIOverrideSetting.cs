292d410bac6f2d2c1fc97017261c52ec24e555c6
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-09-14 19:06:59 +0900

    update : pause할 때 다른 UI 상호작용 가능하도록 수정

--- Assets/Scripts/UI/Override/UIOverrideSetting.cs
+++ Assets/Scripts/UI/Override/UIOverrideSetting.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIOverrideSetting : UIBase
@@ -120,5 +122,23 @@ public class UIOverrideSetting : UIBase
     public void OnBackBtnClicked()
     {
         SetActive<UIOverrideSetting>(false);
+    }
+
+    public void OnHomeClicked()
+    {
+        UIManager.Show<UIOverrideModal>(
+            "메인 메뉴로",
+            "정말 메인 메뉴로 돌아가시겠습니까?",
+            true,
+            true,
+            (Action)BackToMenu
+        );
+
+    }
+
+    private void BackToMenu()
+    {
+        SaveManager.Instance.SaveSlot(0);
+        SceneManager.LoadScene("StartScene");
     }
 }
