0c16d671f6fa87fd5e3ba5e341172d5ff2dc5eb9
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-09-14 12:42:05 +0900

    update : 게임 2배속 UI 변경

--- Assets/Scripts/UI/SpeedController.cs
+++ Assets/Scripts/UI/SpeedController.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedController : MonoBehaviour
 {
-    [SerializeField] GameObject _imageFast;
+    [SerializeField] private Outline _imageFast;
 
     private bool _isFast;
 
     public void OnClickTogleSpeedButton()
     {
         _isFast = !_isFast;
-        _imageFast.SetActive(_isFast);
+        _imageFast.enabled = !_imageFast.enabled;
         Time.timeScale = _isFast ? 2.0f : 1.0f;
     }
 }
